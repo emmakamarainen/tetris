c8a9a1a3e4054772b9ea2d5e1fbd8e6faa343bac
EMMA <emma.kamarainen@helsinki.fi>
2014-09-02 22:27:41 +0300

    muokkausta

---
+++ Tetris/src/main/java/palikkapeli/tetris/App.java
@@ -0,0 +1,13 @@
+package palikkapeli.tetris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
